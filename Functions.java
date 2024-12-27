84d95c2abe1272a910ce6df2294ea7d57f6f2fac
Dhewarsh <badridhewarsh1234@gmail.com>
2024-12-27 15:56:46 +0530

    This is my 2nd commit

---
+++ Functions.java
@@ -0,0 +1,10 @@
+public class Functions{
+    public static int add (int a ,int b){
+        return a+b;
+    }
+    public static void main(String[] args){  
+       
+  System.out.println("Sum of a and b = "  + add(15,25));
+           
+    }
+}
